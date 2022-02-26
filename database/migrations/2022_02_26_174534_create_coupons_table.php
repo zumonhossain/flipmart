a3526b9e4cea66eef2ad3b1a0320e80620c59ed2
“zumonhossain” <zumon15-13169@diu.edu.bd>
2022-02-26 23:48:35 +0600

    Coupon Controller & Database Create

---
+++ database/migrations/2022_02_26_174534_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('coupon_name')->nullable();
+            $table->integer('coupon_discount')->nullable();
+            $table->string('coupon_validity')->nullable();
+            $table->string('coupon_slug')->nullable();
+            $table->integer('coupon_creator')->nullable();
+            $table->integer('coupon_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
