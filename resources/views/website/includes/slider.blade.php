10b69c1c44251c7afba5759cee773bbccd06219e
Zumon Hossain <zumon15-13169@diu.edu.bd>
2022-03-06 12:31:32 +0600

    Slider Show

--- resources/views/website/includes/slider.blade.php
+++ resources/views/website/includes/slider.blade.php
@@ -1,37 +1,23 @@
 <div id="hero">
-				<div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
-					<div class="item" style="background-image: url({{ asset('contents/website') }}/assets/images/sliders/01.jpg);">
-						<div class="container-fluid">
-							<div class="caption bg-color vertical-center text-left">
-								<div class="slider-header fadeInDown-1">Top Brands</div>
-								<div class="big-text fadeInDown-1">
-									New Collections
-								</div>
-								<div class="excerpt fadeInDown-2 hidden-xs">
-									<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
-								</div>
-								<div class="button-holder fadeInDown-3">
-									<a href="index6c11.html?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
-								</div>
-							</div><!-- /.caption -->
-						</div><!-- /.container-fluid -->
-					</div><!-- /.item -->
-
-					<div class="item" style="background-image: url({{ asset('contents/website') }}/assets/images/sliders/02.jpg);">
-						<div class="container-fluid">
-							<div class="caption bg-color vertical-center text-left">
-							<div class="slider-header fadeInDown-1">Spring 2016</div>
-								<div class="big-text fadeInDown-1">
-									Women <span class="highlight">Fashion</span>
-								</div>
-								<div class="excerpt fadeInDown-2 hidden-xs"> 
-									<span>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</span>
-								</div>
-								<div class="button-holder fadeInDown-3">
-									<a href="index6c11.html?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
-								</div>
-							</div><!-- /.caption -->
-						</div><!-- /.container-fluid -->
-					</div><!-- /.item -->
-				</div><!-- /.owl-carousel -->
-			</div>
+	<div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
+		@php
+			$banners = App\Models\Banner::where('ban_status',1)->orderBy('id','DESC')->limit(3)->get();
+		@endphp
+		@foreach ($banners as $banner)
+			<div class="item" style="background-image: url({{ asset($banner->ban_image) }});">
+				<div class="container-fluid">
+					<div class="caption bg-color vertical-center text-left">
+						<div class="slider-header fadeInDown-1">{{ $banner->ban_title }}</div>
+						<div class="big-text fadeInDown-1">{{ $banner->ban_subtitle }}</div>
+						<div class="excerpt fadeInDown-2 hidden-xs">
+							<span>{{ $banner->ban_description }}</span>
+						</div>
+						<div class="button-holder fadeInDown-3">
+							<a href="{{ $banner->ban_url }}" class="btn-lg btn btn-uppercase btn-primary shop-now-button">{{ $banner->ban_button }}</a>
+						</div>
+					</div><!-- /.caption -->
+				</div><!-- /.container-fluid -->
+			</div><!-- /.item -->
+		@endforeach
+	</div><!-- /.owl-carousel -->
+</div>
